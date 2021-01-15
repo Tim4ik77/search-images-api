f9f7cefa99d32706075d9d1879701ac38798fe1c
Tim4ik77 <50327888+Tim4ik77@users.noreply.github.com>
2021-01-15 13:48:43 +0300

    Update search.py

--- search.py
+++ search.py
@@ -2,27 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
+def create_dir(dir):
+    try:
+        os.makedir(dir)
+    else:
+        pass
+
 def main():
-    n = int(input("Введите кол-во страниц: "))
-    q = input("Введите поисковый запрос: ")
-    q1 = eval(q)
-    for q in q1:
-        url = "https://yandex.ru/images/search?text={}".format(q)
-        try:
-            os.mkdir(q)
-        except:
-            pass
-        number = 0
-        for n1 in range(n):
-            page = requests.get(url+"&p={}".format(n1))
-            soup = BeautifulSoup(page.text, "html.parser")
-            pictures = soup.findAll(class_='serp-item__thumb justifier__thumb')
-            for picture in pictures:
-                picture = 'http:'+picture.get('src')
-                picture = requests.get(picture)
-                with open("./{}/{}.png".format(q, number), "wb") as code:
-                    code.write(picture.content)
-                number+=1
+    pages = int(input("Enter the number of pages: "))
+    q = input("Enter your search query: ")
+    create_dir(q)
+    url = "https://yandex.ru/images/search?text={}".format(q)
+    number = 0
+    for p in range(pages):
+        page = requests.get(url+"&p={}".format(p))
+        soup = BeautifulSoup(page.text, "html.parser")
+        pictures = soup.findAll(class_='serp-item__thumb justifier__thumb')
+        for picture in pictures:
+            picture = 'http:'+picture.get('src')
+            picture = requests.get(picture)
+            with open("./{}/{}.png".format(q, number), "wb") as code:
+                code.write(picture.content)
+            number+=1
 
 
 
